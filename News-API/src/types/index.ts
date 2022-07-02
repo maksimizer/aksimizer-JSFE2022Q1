c86b61200dd2006a873801e599a9f15e081a942e
maksimizer <maximorlov@mail.ru>
2022-07-02 23:06:01 +0300

    feat: add some interfaces to index.ts

---
+++ News-API/src/types/index.ts
@@ -0,0 +1,35 @@
+export interface IEndpointSources {
+  status: string;
+  sources: ISourceExtended[];
+
+}
+
+export interface IEndpointEverything {
+  status: string;
+  totalResults: number;
+  articles: IArticle[];
+}
+
+export interface ISource {
+  id: string;
+  name: string;
+}
+
+export interface ISourceExtended extends ISource {
+  description: string;
+  url: string;
+  category: string;
+  language: string;
+  country: string;
+}
+
+export interface IArticle {
+  source: ISource;
+  autthor: string;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage: string;
+  publishedAt:  string;
+  content: string;
+}
