3fe7655daf5a0251be1c34bd0496b4e3905f3362
maksimizer <maximorlov@mail.ru>
2022-07-03 01:32:29 +0300

    refactor: refactor loader.js to ts

---
+++ News-API/src/components/controller/loader.ts
@@ -0,0 +1,48 @@
+import { ILoaderOptions } from '../../types/index';
+
+class Loader {
+    private _baseLink: string;
+    private _options: ILoaderOptions;
+    constructor(baseLink: string, options: ILoaderOptions) {
+        this._baseLink = baseLink;
+        this._options = options;
+    }
+
+    getResp(
+        { endpoint, options = {} }: { endpoint: string; options: { sources?: string } },
+        callback = (): void => {
+            console.error('No callback for GET response');
+        }
+    ) {
+        this.load('GET', endpoint, callback, options);
+    }
+
+    errorHandler(res: Response): Response {
+        if (!res.ok) {
+            if (res.status === 401 || res.status === 404)
+                console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
+            throw Error(res.statusText);
+        }
+        return res;
+    }
+
+    makeUrl(options: { sources?: string }, endpoint: string): string {
+        const urlOptions: { [index: string]: string } = { ...this._options, ...options };
+        let url = `${this._baseLink}${endpoint}?`;
+
+        Object.keys(urlOptions).forEach((key) => {
+            url += `${key}=${urlOptions[key]}&`;
+        });
+        return url.slice(0, -1);
+    }
+
+    load<U>(method: string, endpoint: string, callback: (data: U) => void, options = {}) {
+        fetch(this.makeUrl(options, endpoint), { method })
+            .then(this.errorHandler)
+            .then((res) => res.json())
+            .then((data) => callback(data))
+            .catch((err) => console.error(err));
+    }
+}
+
+export default Loader;
