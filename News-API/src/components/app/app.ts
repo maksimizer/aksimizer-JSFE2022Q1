2cb5056a869c593b3b3e8afa31d03c50a93e526f
maksimizer <maximorlov@mail.ru>
2022-07-03 22:25:44 +0300

    refactor: refactor app.js t ots

---
+++ News-API/src/components/app/app.ts
@@ -0,0 +1,21 @@
+import AppController from '../controller/controller';
+import { AppView } from '../view/appView';
+
+class App {
+    private _controller: AppController;
+    private _view: AppView;
+    constructor() {
+        this._controller = new AppController();
+        this._view = new AppView();
+    }
+
+    start() {
+        const sources = document.querySelector('.sources');
+        if (sources) {
+            sources.addEventListener('click', (e) => this._controller.getNews(e, (data) => this._view.drawNews(data)));
+        }
+        this._controller.getSources((data) => this._view.drawSources(data));
+    }
+}
+
+export default App;
