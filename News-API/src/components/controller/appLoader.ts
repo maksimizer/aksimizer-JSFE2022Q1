7daac2833828f02d911cdb5ed5a1ad77311ef5b6
maksimizer <maximorlov@mail.ru>
2022-07-03 22:43:02 +0300

    fix: add new link to appLoader

--- News-API/src/components/controller/appLoader.ts
+++ News-API/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '8a7e577611d942bd85b253da371bd072', // получите свой ключ https://newsapi.org/
         });
     }
