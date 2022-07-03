257fc01079f7c35142277857b36a7b042f84827a
maksimizer <maximorlov@mail.ru>
2022-07-03 15:03:53 +0300

    fix: fix generic types in loader.ts

--- News-API/src/components/controller/loader.ts
+++ News-API/src/components/controller/loader.ts
@@ -8,12 +8,9 @@ class Loader {
         this._options = options;
     }
 
-    getResp(
+    getResp<T>(
         { endpoint, options = {} }: { endpoint: string; options?: { sources?: string } },
-        callback = (): void => {
-            console.error('No callback for GET response');
-        }
-    ) {
+        callback:(data: T) => void): void {
         this.load('GET', endpoint, callback, options);
     }
 
@@ -36,7 +33,7 @@ class Loader {
         return url.slice(0, -1);
     }
 
-    load<U>(method: string, endpoint: string, callback: (data: U) => void, options = {}) {
+    load<T>(method: string, endpoint: string, callback: (data: T) => void, options = {}) {
         fetch(this.makeUrl(options, endpoint), { method })
             .then(this.errorHandler)
             .then((res) => res.json())
