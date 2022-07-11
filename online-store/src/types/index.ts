36955cd3bafc483958d8e22d9ed4b385c20eee50
maksimizer <maximorlov@mail.ru>
2022-07-11 20:41:26 +0300

    feat: add data.ts file and interface for goods item

---
+++ online-store/src/types/index.ts
@@ -0,0 +1,10 @@
+export interface Boat {
+    num: string,
+    name: string,
+    quantity: string,
+    year: string,
+    producer: string,
+    color: string,
+    size: string,
+    favorite: boolean,
+}
