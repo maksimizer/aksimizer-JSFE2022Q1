5c2e6ca4721173b486324528623c1b10c7e40a5f
maksimizer <maximorlov@mail.ru>
2022-07-16 20:37:31 +0300

    feat: add clear search field button

--- online-store/src/js/search.ts
+++ online-store/src/js/search.ts
@@ -1,8 +1,11 @@
 export function addSearch() {
     searchField.addEventListener('keyup', search);
+    clearBtn.addEventListener('click', clearSearchField);
 }
 
 const searchField = document.querySelector('.search') as HTMLInputElement;
+const clearBtn = document.querySelector('.clear-search-button') as HTMLInputElement;
+
 export function search() {
     const names = document.querySelectorAll('.item-name');
     names.forEach((el) => {
@@ -10,10 +13,22 @@ export function search() {
         const card = el.parentElement as HTMLElement;
         if (searchField.value && name.toLowerCase().includes(`${searchField.value.toLowerCase()}`)) {
             card.classList.remove('search-hidden');
+            clearBtn.classList.remove('hidden');
+            searchField.style.backgroundImage = 'none';
         } else if (searchField.value && !name.toLowerCase().includes(`${searchField.value.toLowerCase()}`)) {
             card.classList.add('search-hidden');
+            clearBtn.classList.remove('hidden');
+            searchField.style.backgroundImage = 'none';
         } else if (!searchField.value) {
             card.classList.remove('search-hidden');
+            clearBtn.classList.add('hidden');
+            searchField.style.backgroundImage = 'url(./assets/img/search.svg)';
         }
     });
+}
+
+function clearSearchField() {
+    searchField.value = '';
+    searchField.focus();
+    search();
 }
