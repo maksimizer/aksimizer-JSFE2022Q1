c73e9beafe92f058f512b129f2ea0d6474bd2d54
maksimizer <maximorlov@mail.ru>
2022-08-08 23:31:10 +0300

    feat: add alert

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -6,3 +6,5 @@ import WinnersController from './controllers/winnersController';
 const navigationController = new NavigationController(); // eslint-disable-line
 const garageController = new GarageController(); // eslint-disable-line
 const winnersController = new WinnersController(); // eslint-disable-line
+
+alert('Привет, дорогой друг! Проверь пожалуйста меня в среду вечером. Очень надеюсь на твою доброту!. Спасибо! ') // eslint-disable-line
